0e43b1e3b589a3ef0c8880ad542362c28835471a
Yayehyrad <yayehyrad.sisay@gmail.com>
2024-10-15 22:33:55 +0300

    sign in form handling error and zod and hookform integration

--- front_end/src/forms/signinform.tsx
+++ front_end/src/forms/signinform.tsx
@@ -1,12 +1,27 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import {useForm} from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
 import React from 'react'
+import { formDefaultValue, formSchema, IFormSchma } from './formSchema'
 
 const SigninForm = () => {
+  const { register , formState : {errors , isLoading } , reset , handleSubmit } = useForm<IFormSchma>({
+    mode : "onBlur",
+    resolver : zodResolver(formSchema),
+    defaultValues : formDefaultValue
+    
+  })
+  const handleFormSubmit = (data : IFormSchma) => {
+     console.log(data);
+     reset()
+  }
   return (
-    <form>
-    <Input type='text' placeholder='User name' className=' h-14 mb-4 text-2xl text-white '/>
-    <Input type="password" placeholder='password' className=' h-14 mb-4 text-2xl  text-white'  />
+    <form onSubmit={handleSubmit(handleFormSubmit)}>
+    <Input type='text' placeholder='User name' className=' h-14 mb-4 text-2xl text-white ' {...register("userName" )}/>
+    {errors.userName && errorText(errors.userName?.message)}
+    <Input type="password" placeholder='password' className=' h-14 mb-4 text-2xl  text-white'  {...register("password")} />
+    {errors.password && errorText(errors.password?.message)}
     <div className=' flex flex-row  w-full   justify-start items-center'>
     <input type="checkbox" className=' appearance-none w-6 h-6 border-2 border-gray-100 rounded-lg checked:bg-gray-500 checked:border-transparent focus:outline-none cursor-pointer' />
     <label htmlFor="remember" className=' ml-2 text-xl italic font-semibold text-white'>Remember me</label>
@@ -16,6 +31,12 @@ const SigninForm = () => {
     </div>
 </form>
   )
+}
+
+const errorText = ( text : string | undefined )=>{
+  return <div className=' text-red-400 '>
+    {text}
+  </div>
 }
 
 export default SigninForm
