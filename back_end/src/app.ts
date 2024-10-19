0bfae911e885dadc7b56137b5d8d57018aeed5da
Yayehyrad <yayehyrad.sisay@gmail.com>
2024-10-20 01:18:36 +0300

    config app for cors

--- back_end/src/app.ts
+++ back_end/src/app.ts
@@ -7,11 +7,11 @@ import userRouter from "./routes/user/user.route";
 const app = express();
 
 app.use(cors(
-    {
-        origin : process.env.CLIENT_URL,
-        methods : ["GET" , 'POST' , 'PUT' , 'DELETE'],
-        allowedHeaders: ["Content-Type" , "Authorization"]
-    }
+    // {
+    //     origin : process.env.CLIENT_URL,
+    //     methods : ["GET" , 'POST' , 'PUT' , 'DELETE'],
+    //     allowedHeaders: ["Content-Type" , "Authorization"]
+    // }
 ));
 app.use(bodyParser.json());
 app.use(cookieParser());
