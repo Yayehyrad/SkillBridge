2819d10fc8ab0fe0c630c8c786ee1a87f468b0fd
Yayehyrad <yayehyrad.sisay@gmail.com>
2024-10-24 19:50:46 +0300

    set route

--- back_end/src/app.ts
+++ back_end/src/app.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user/user.route";
 import courseRouter from "./routes/instructor/instructor.course.route";
+import mediaRouter  from "./routes/instructor/instructor.media";
+
 
 
 const app = express();
@@ -19,6 +21,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use('/api/v1/users' , userRouter)
 app.use('/api/v1/course' , courseRouter)
+app.use('/api/v1/instructor' , mediaRouter)
 // app.get("/", (req , res) => {
 //   res.send("Hello World!");
 // });
